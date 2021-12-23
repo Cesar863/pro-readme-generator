388d6d464b9919d92d65e41649828e60d8e156f2
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2021-12-23 18:10:22 -0500

    added function to initiate questions

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const questions = [
         type: 'list',
         name: 'license',
         message: 'What type of license is used in your project?',
-        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear','cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl','ecl-2.0','epl-1.0','epl-2.0','eupl-1.1','agpl-3.0','gpl','gpl-2.0','gpl-3.0','lgpl','lgpl-2.1','lgpl-3.0','isc','lppl-1.3c','ms-pl','mit','mpl-2.0','osl-3.0','postgresql','ofl-1.1','ncsa','unlicense', 'zlib',
+        choices: ['none','afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear','cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl','ecl-2.0','epl-1.0','epl-2.0','eupl-1.1','agpl-3.0','gpl','gpl-2.0','gpl-3.0','lgpl','lgpl-2.1','lgpl-3.0','isc','lppl-1.3c','ms-pl','mit','mpl-2.0','osl-3.0','postgresql','ofl-1.1','ncsa','unlicense', 'zlib',
         ]
     },
     {
@@ -60,7 +60,9 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
