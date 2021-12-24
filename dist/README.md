25ae9af8c64d9d7c30c647473cc4829f489b2373
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2021-12-23 21:35:14 -0500

    finished project logic now read me generates

---
+++ dist/README.md
@@ -0,0 +1,56 @@
+# ttt
+![GitHub license] (https://img.shields.io/badge/license-none-yellowgreen.svg)
+
+## Github URL
+
+[sdf](https://github.com/sdf/)
+
+## Description
+
+cedss
+
+## Table of Contents
+
+* [Installations](#dependencies)
+
+*[usage](#usage)
+
+
+* [License](#license)
+
+
+*[Contributors](#contributors)
+
+*[Test](*test)
+
+## Installations (Packages)
+
+To install dependencies, please run these commands:
+
+fefe
+
+## Repo Use
+
+ytht
+
+## License 
+      
+      Copyright © none. All rights reserved.
+      
+      Licensed under the none license.
+
+## Contributors
+
+erf
+
+Contact me at undefined
+
+## Tests
+
+To run tests, please run the following commands:
+
+node test
+
+## Created by 
+
+qwe
--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const questions = [
         type: 'list',
         name: 'license',
         message: 'What type of license is used in your project?',
-        choices: ['none','afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear','cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl','ecl-2.0','epl-1.0','epl-2.0','eupl-1.1','agpl-3.0','gpl','gpl-2.0','gpl-3.0','lgpl','lgpl-2.1','lgpl-3.0','isc','lppl-1.3c','ms-pl','mit','mpl-2.0','osl-3.0','postgresql','ofl-1.1','ncsa','unlicense', 'zlib',
+        choices: ['none','afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear','cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl','ecl-2.0','epl-1.0','epl-2.0','eupl-1.1','agpl-3.0','gpl','gpl-2.0','gpl-3.0','lgpl','lgpl-2.1','lgpl-3.0','isc','lppl-1.3c','ms-pl','mit','mpl-2.0','osl-3.0','postgresql','ofl-1.1','ncsa', 'zlib',
         ]
     },
     {
@@ -57,11 +57,31 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+//     return fs.writeFile(path.join(process.cwd(), fileName), data);
+// }
+const writeToFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/README.md', fileContent, err => {
+            if(err) {
+                reject(err);
+                return;
+            }
+            resolve ({
+                okay:true,
+                message: 'File Created'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
+        .then ((inquirerAnswers) => {
+            console.log("Please wait while your README is being generated")
+            writeToFile(generateMarkdown({ ...inquirerAnswers }));
+        });
 }
 
 // Function call to initialize app
